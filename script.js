5b4447808dea43e91022ebfb2f34dda6905ebc2c
kimagure <konosuke.20041116@gmail.com>
2025-12-21 00:27:56 +0900

    トランプに

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const dealBtn = document.getElementById("deal");
-const field = document.getElementById("field");
+const slots = document.querySelectorAll(".slot");
 
-const cards = [1, 2, 3, 4, 5,6];
+// ♤1〜13を2枚ずつ（26枚）
+let cards = [];
+for (let num = 1; num <= 13; num++) {
+    cards.push("♤" + num);
+    cards.push("♤" + num);
+}
 
-dealBtn.addEventListener("click", function () {
-    field.innerHTML = "";
+// 各列に配る枚数
+const counts = [3, 3, 3, 3, 3, 3, 2, 2, 2, 2];
 
-    for (let i = 0; i < cards.length; i++) {
+// 枠にカードを配置（表向き）
+let index = 0;
+
+for (let i = 0; i < slots.length; i++) {
+    for (let j = 0; j < counts[i]; j++) {
         const cardDiv = document.createElement("div");
         cardDiv.className = "card";
-        cardDiv.textContent = cards[i];
-        field.appendChild(cardDiv);
+        cardDiv.textContent = cards[index];
+        slots[i].appendChild(cardDiv);
+        index++;
     }
-});
+}
